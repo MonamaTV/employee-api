39ba8a1eb64e8ed884e27a233fbf90980ab848c8
tmonama023 <tmonama023@student.wethinkcode.co.za>
2023-11-15 12:20:12 +0200

    nodemailer

---
+++ mail.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+// Create a Nodemailer transporter with SMTP details
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "unique.howell@ethereal.email",
+    pass: "gwZZg82EpyNwUP4cWZ",
+  },
+});
+
+// Define the email options
+let mailOptions = {
+  from: "unique.howell@ethereal.email",
+  to: "keletsovincent92@gmail.com",
+  subject: "Testing Nodemailer with Gmail using host",
+  text: "This is a test email sent using Nodemailer with Gmail via host settings.",
+};
+
+// Send the email
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info);
+  }
+});
